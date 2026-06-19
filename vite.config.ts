c47961f82ccf26e9ea08bd176ed8184636fa62eb
zheglo <sergeyzheglo@gmail.com>
2026-06-19 18:51:02 +0300

    -unocss/transformer-directives

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import UnoCSS from '@unocss/svelte-scoped/vite'
-import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
 	plugins: [
-		UnoCSS({
-			cssFileTransformers: [transformerDirectives()],
-    }),
+		UnoCSS(),
 		sveltekit()]
 });
