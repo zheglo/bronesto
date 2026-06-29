f78a397898c5e89906a53b978987a1a000d0b8c5
zheglo <sergeyzheglo@gmail.com>
2026-06-30 00:47:46 +0300

    pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,35 +1,24 @@
+
 module.exports = {
-  apps : [{
-    name: 'bronesto',
-    cwd: '/home/zheglo/bronesto',
-    script: '/home/zheglo/bronesto/build/index.js',
-    exec_mode: 'cluster',
-    watch: false,
-  }],
-    env: {
-        NODE_ENV: "production",
+  apps: [
+    {
+      name: 'bronesto',
+      cwd: '/home/zheglo/bronesto',
+      script: 'build/index.js',
+      exec_mode: 'cluster',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
         PORT: 3000,
       },
-
-    autorestart: true,
-    max_restarts: 10,
-    min_uptime: "10s",
-    max_memory_restart: "500M",
-    restart_delay: 5000,
-
-    kill_timeout: 5000,
-    wait_ready: true,
-    listen_timeout: 10000,
-
-  deploy: {
-    production: {
-      user: "zheglor",
-      host: "bronesto.fun",
-      ref: "origin/master",
-      repo: "https://github.com/zheglo/bronesto.git",
-      path: "/home/zheglo/bronesto",
-      "post-deploy":
-        "pnpm install && pnpm run build && pm2 reload ecosystem.config.cjs",
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+      max_memory_restart: '500M',
+      restart_delay: 5000,
+      kill_timeout: 5000,
+      wait_ready: true,
+      listen_timeout: 10000,
     },
-  }
-}
+  ],
+};
